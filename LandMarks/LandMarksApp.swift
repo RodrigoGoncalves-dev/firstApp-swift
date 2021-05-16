918f1c96f75e4c5855cab7b5d4ed546c12f76c8a
rodrigo <rodrigo.goncalves2001r@gmail.com>
2021-05-16 01:27:47 -0300

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Rodrigo Gonçalves on 16/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
