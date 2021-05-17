ac9f1044baa25b173660e1011a8b2a872b9f60cc
rodrigo <rodrigo.goncalves2001r@gmail.com>
2021-05-17 00:52:08 -0300

    first commit

---
+++ LandMarks/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by Rodrigo Gonçalves on 16/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
