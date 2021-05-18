24011fe04a91b359690da10df05217f59fcd591b
rodrigo <rodrigo.goncalves2001r@gmail.com>
2021-05-18 00:36:30 -0300

    created file

---
+++ LandMarks/Badge/HikeBadge.swift
@@ -0,0 +1,20 @@
+//
+//  HikeBadge.swift
+//  LandMarks
+//
+//  Created by Rodrigo Gonçalves on 17/05/21.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge()
+    }
+}
---
+++ LandMarks/Model/Profile.swift
@@ -0,0 +1,8 @@
+//
+//  Profile.swift
+//  LandMarks
+//
+//  Created by Rodrigo Gonçalves on 17/05/21.
+//
+
+import Foundation
